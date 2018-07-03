aa836a2b588123fab2666fe91c314f955581bc27
Conor Hamilton <conhamil19@gmail.com>
2018-07-03 17:53:39 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.text :image_url
+      t.string :size 
+      t.timestamps
+    end 
+  end 
+
+end 
