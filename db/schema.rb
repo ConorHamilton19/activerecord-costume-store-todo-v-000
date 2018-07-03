c6736c6eef3d111b7df7af780e48095b1a2c8b4b
Conor Hamilton <conhamil19@gmail.com>
2018-07-03 17:58:25 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -18,10 +18,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.integer "costume_inventory"
     t.integer "num_of_employees"
     t.boolean "still_in_business"
-    t.text "image_url"
-    t.string "size"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "opening_time"
+    t.datetime "closing_time"
   end
 
   create_table "costumes", force: :cascade do |t|
@@ -36,13 +34,12 @@ ActiveRecord::Schema.define(version: 3) do
   create_table "haunted_houses", force: :cascade do |t|
     t.string "name"
     t.string "location"
-    t.integer "costume_inventory"
-    t.integer "num_of_employees"
-    t.boolean "still_in_business"
-    t.text "image_url"
-    t.string "size"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "theme"
+    t.float "price"
+    t.boolean "family_friendly"
+    t.datetime "opening_date"
+    t.datetime "closing_date"
+    t.text "description"
   end
 
 end
